8654639a6323c7f7a663200c272d90247d1969c8
Qpyy <boom8008@yandex.ru>
2024-11-22 12:53:28 +0300

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import PumpSelection from "./pages/PumpSelection";
 import Result from "./pages/Result";
 
@@ -7,6 +7,7 @@ function App() {
     <Routes>
       <Route path="/" element={<PumpSelection />} />
       <Route path="/results" element={<Result />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
